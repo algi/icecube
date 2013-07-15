8bfec1bceacc7cb9cb3f2933ecfab63ad9107406
Marian Bouček <boucekm@gmail.com>
2013-07-15 23:49:12 +0200

    Stub for task controller

---
+++ IceCube/Controllers/MBTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBTaskViewController.h
+//  IceCube
+//
+//  Created by Marian Bouček on 15.07.13.
+//  Copyright (c) 2013 Marian Bouček. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBTaskViewController : NSObject
+
+@end
---
+++ IceCube/Controllers/MBTaskViewController.m
@@ -0,0 +1,13 @@
+//
+//  MBTaskViewController.m
+//  IceCube
+//
+//  Created by Marian Bouček on 15.07.13.
+//  Copyright (c) 2013 Marian Bouček. All rights reserved.
+//
+
+#import "MBTaskViewController.h"
+
+@implementation MBTaskViewController
+
+@end
