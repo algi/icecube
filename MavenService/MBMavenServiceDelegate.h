2979ad3850aa9d83aca11e5fd55f0900a09ad254
Marian Bouček <boucekm@gmail.com>
2013-09-19 20:55:22 +0200

    Added XPC delegate
    — and build target reset to 10.8

---
+++ MavenService/MBMavenServiceDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MBMavenServiceDelegate.h
+//  IceCube
+//
+//  Created by Marian Bouček on 19.09.13.
+//  Copyright (c) 2013 Marian Bouček. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBMavenServiceDelegate : NSObject <NSXPCListenerDelegate>
+
+@end
---
+++ MavenService/MBMavenServiceDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MBMavenServiceDelegate.m
+//  IceCube
+//
+//  Created by Marian Bouček on 19.09.13.
+//  Copyright (c) 2013 Marian Bouček. All rights reserved.
+//
+
+#import "MBMavenServiceDelegate.h"
+
+@implementation MBMavenServiceDelegate
+
+@end
