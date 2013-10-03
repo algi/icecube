72e2ca2d41f81b81a937f64990ba15921a865e8d
Marian Bouček <boucekm@gmail.com>
2013-10-03 10:23:27 +0200

    Setup Maven and Java home after start
    — it also spawns XPC when tests are launched…

--- IceCube/MBAppDelegate.m
+++ IceCube/MBAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "MBAppDelegate.h"
 
+#import "MBJavaHomeService.h"
 #import "MBPreferencesWindowController.h"
 
 @interface MBAppDelegate ()
@@ -17,6 +18,44 @@
 @end
 
 @implementation MBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
+{
+	// update paths in background
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+		
+		// set default Maven home if needed
+		NSString *mavenHome = [[NSUserDefaults standardUserDefaults] objectForKey:@"maven.application.path"];
+		if (!mavenHome) {
+			[[NSUserDefaults standardUserDefaults] setObject:@"/usr/bin/mvn" forKey:@"maven.application.path"];
+		}
+		
+		// renew JAVA_HOME if needed
+		NSString *javaVersion = nil;
+		BOOL isUserSpecified = NO;
+		
+		NSNumber *javaSelectedIndex = [[NSUserDefaults standardUserDefaults] objectForKey:@"java.selected.index"];
+		if ([javaSelectedIndex integerValue] == 1) {
+			isUserSpecified = YES;
+		}
+		else if ([javaSelectedIndex integerValue] > 5) {
+			// index specifies version of Java (eg 6 = Java 6)
+			javaVersion = [javaSelectedIndex stringValue];
+		}
+		
+		if (!isUserSpecified) {
+			NSXPCConnection *connection = [[NSXPCConnection alloc] initWithServiceName:@"cz.boucekm.JavaHomeService"];
+			connection.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(MBJavaHomeService)];
+			
+			[connection resume];
+			
+			[[connection remoteObjectProxy] findJavaLocationForVersion:javaVersion withReply:^(NSString *result) {
+				[[NSUserDefaults standardUserDefaults] setObject:result forKey:@"java.home.path"];
+				[connection invalidate];
+			}];
+		}
+	});
+}
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
 {
