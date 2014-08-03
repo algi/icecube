f23567ed89d6c1844aff9d6ae89aeec60d5eff8a
Marian Boucek <marian@Powerbolt.local>
2014-08-03 21:06:58 +0100

    Fixed incorrect activity in XPC for fetching Java home

--- IceCube/MBAppDelegate.m
+++ IceCube/MBAppDelegate.m
@@ -21,33 +21,34 @@
 
 - (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
+    id activity = [[NSProcessInfo processInfo] beginActivityWithOptions:NSActivityBackground reason:@"Fetching default JavaHome"];
+
 	// register default values for Maven and Java
-	[[NSProcessInfo processInfo] performActivityWithOptions:NSActivityBackground reason:@"Fetching default JavaHome" usingBlock:^{
-		NSXPCConnection *connection = [[NSXPCConnection alloc] initWithServiceName:@"cz.boucekm.JavaHomeService"];
-		[connection setRemoteObjectInterface:[NSXPCInterface interfaceWithProtocol:@protocol(MBJavaHomeService)]];
-		[connection resume];
-		
-		id<MBJavaHomeService> remoteProxy = [connection remoteObjectProxy];
-		[remoteProxy findDefaultJavaHome:^(NSString *defaultJavaHome, NSError *error) {
-			
-			if (!defaultJavaHome) {
-				
-				// present error to user
-				dispatch_sync(dispatch_get_main_queue(), ^{
-					[NSApp presentError:error];
-				});
-				
-				// fall back to default Java home (which even doesn't need to exist)
-				defaultJavaHome = @"/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home";
-			}
-			
-			NSDictionary *userDefaults = @{@"maven.home": @"/usr/bin/mvn",
-										   @"java.home": defaultJavaHome};
-			[[NSUserDefaults standardUserDefaults] registerDefaults:userDefaults];
-			
-			[connection invalidate];
-		}];
-	}];
+    NSXPCConnection *connection = [[NSXPCConnection alloc] initWithServiceName:@"cz.boucekm.JavaHomeService"];
+    [connection setRemoteObjectInterface:[NSXPCInterface interfaceWithProtocol:@protocol(MBJavaHomeService)]];
+    [connection resume];
+    
+    id<MBJavaHomeService> remoteProxy = [connection remoteObjectProxy];
+    [remoteProxy findDefaultJavaHome:^(NSString *defaultJavaHome, NSError *error) {
+
+        if (!defaultJavaHome) {
+            
+            // present error to user
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                [NSApp presentError:error];
+            });
+            
+            // fall back to default Java home (which even doesn't need to exist)
+            defaultJavaHome = @"/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home";
+        }
+        
+        NSDictionary *userDefaults = @{@"maven.home": @"/usr/bin/mvn",
+                                       @"java.home": defaultJavaHome};
+        [[NSUserDefaults standardUserDefaults] registerDefaults:userDefaults];
+
+        [[NSProcessInfo processInfo] endActivity:activity];
+        [connection invalidate];
+    }];
 }
 
 - (IBAction)showPreferences:(id)sender
