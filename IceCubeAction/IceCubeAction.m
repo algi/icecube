9400203ae7289d3a9d96baf084ee2bbcda6bec62
Marian Boucek <boucekm@gmail.com>
2026-02-15 14:39:37 +0100

    Define custom macro instead of redefining the existing one

--- IceCubeAction/IceCubeAction.m
+++ IceCubeAction/IceCubeAction.m
@@ -8,10 +8,9 @@
 
 #import "IceCubeAction.h"
 
-// redefine our own version of localized string lookup
+// define our own version of localized string lookup
 // we need to look in our own bundle, not main application bundle
-#undef NSLocalizedString
-#define NSLocalizedString(key, comment) \
+#define MBLocalizedString(key, comment) \
     [[self bundle] localizedStringForKey:(key) value:@"" table:nil]
 
 @implementation IceCubeAction
@@ -26,7 +25,7 @@ static NSString * const kDefaultMavenPath = @"/usr/share/maven/bin/mvn";
     if (mavenCommand == nil) {
         mavenCommand = kDefaultMavenCommand;
 
-        NSString *message = NSLocalizedString(@"No Maven command specified, will use default: %@",
+        NSString *message = MBLocalizedString(@"No Maven command specified, will use default: %@",
                                               @"Default {Maven command} will be used instead of empty command.");
         [self logMessageWithLevel:AMLogLevelInfo format:message, kDefaultMavenCommand];
     }
@@ -36,14 +35,14 @@ static NSString * const kDefaultMavenPath = @"/usr/share/maven/bin/mvn";
     if (mavenPath == nil) {
         mavenPath = kDefaultMavenPath;
 
-        NSString *message = NSLocalizedString(@"No Maven installation directory specified, will use default: %@",
+        NSString *message = MBLocalizedString(@"No Maven installation directory specified, will use default: %@",
                                               @"Default {Maven installation directory} will be used instead of empty path.");
         [self logMessageWithLevel:AMLogLevelInfo format:message, kDefaultMavenPath];
     }
 
     // Error log path
     NSString *errorLogPath = nil;
-    if ([self.useErrorLogging state] == NSOnState) {
+    if ([self.useErrorLogging state] == NSControlStateValueOn) {
         errorLogPath = self.parameters[@"errorLogPath"];
     }
 
@@ -69,7 +68,7 @@ static NSString * const kDefaultMavenPath = @"/usr/share/maven/bin/mvn";
         else {
             [self writeOutputFromTask:task toTargetFolder:errorLogPath];
 
-            NSString *message = NSLocalizedString(@"Build failure for project: %@",
+            NSString *message = MBLocalizedString(@"Build failure for project: %@",
                                                   @"Unable to build Maven project for {path}.");
             [self logMessageWithLevel:AMLogLevelWarn format:message, pomFilePath];
         }
@@ -96,7 +95,7 @@ static NSString * const kDefaultMavenPath = @"/usr/share/maven/bin/mvn";
         return task;
     }
     @catch (NSException *exception) {
-        NSString *message = NSLocalizedString(@"Unable to launch Maven. Reason: %@",
+        NSString *message = MBLocalizedString(@"Unable to launch Maven. Reason: %@",
                                               @"Unable to launch Maven due to {error}.");
         [self logMessageWithLevel:AMLogLevelError format:message, exception.reason];
         return nil;
@@ -119,12 +118,12 @@ static NSString * const kDefaultMavenPath = @"/usr/share/maven/bin/mvn";
     NSError *error = nil;
     BOOL fileCreated = [outputData writeToFile:errorLogPath atomically:YES encoding:NSUTF8StringEncoding error:&error];
     if (fileCreated) {
-        NSString *message = NSLocalizedString(@"Error file created at path: %@",
+        NSString *message = MBLocalizedString(@"Error file created at path: %@",
                                               @"Error file was created at {path}.");
         [self logMessageWithLevel:AMLogLevelInfo format:message, errorLogPath];
     }
     else {
-        NSString *message = NSLocalizedString(@"Unable to create error log at path: %@, reason: %@",
+        NSString *message = MBLocalizedString(@"Unable to create error log at path: %@, reason: %@",
                                               @"Unable to create error log at {path} for {reason}.");
         [self logMessageWithLevel:AMLogLevelWarn format:message, errorLogPath, [error description]];
     }
